231dd3aef68f90e11900c9c9f1ced5928c78ad16
Shane Kadish <skadish@atlassian.com>
2021-11-13 16:06:11 +1100

    part 1 working!

--- labs/1-gpio/code/gpio.c
+++ labs/1-gpio/code/gpio.c
@@ -11,10 +11,30 @@
 
 // see broadcomm documents for magic addresses.
 #define GPIO_BASE_PADDR 0x20200000
+
 static const unsigned int gpio_fsel0 = (GPIO_BASE_PADDR);
 static const unsigned int gpio_set0  = (GPIO_BASE_PADDR + 0x1C);
 static const unsigned int gpio_clr0  = (GPIO_BASE_PADDR + 0x28);
 static const unsigned int gpio_lev0  = (GPIO_BASE_PADDR + 0x34);
+
+typedef uint32_t word_t;
+
+/*
+ * Engler:
+ * "The different GPFSELn registers handle groups of 10, so you can divide the
+ * pin number to compute the right GPFSEL register"
+ *
+ * gpio_fsel0 = { GPIO  0 ... GPIO  9 }
+ * gpio_fsel1 = { GPIO 10 ... GPIO 19 }
+ * gpio_fsel2 = { GPIO 20 ... GPIO 29 }
+ * ...
+ * gpio_fsel5 = { GPIO 50 ... GPIO 53 }
+ *
+ * TODO: How to make sense of the fact that we're on an arch with only a subset of these GPIO pins?
+ */
+static unsigned int get_gpio_fsel_paddr(unsigned int pin) {
+    return gpio_fsel0 + ((pin / 10) * sizeof(word_t));
+}
 
 //
 // Part 1 implement gpio_set_on, gpio_set_off, gpio_set_output
@@ -25,20 +45,25 @@ static const unsigned int gpio_lev0  = (GPIO_BASE_PADDR + 0x34);
 // note: fsel0, fsel1, fsel2 are contiguous in memory, so you 
 // can (and should) use array calculations!
 void gpio_set_output(unsigned int pin) {
-    // implement this
-    // use <gpio_fsel0>
+    // Write '001' to bits [k .. k - 2] of gpio_fseln, **leaving all other bits undisturbed**
+    // TODO: I know that Engler alluded to the undisturbed thing, but where in the broadcom doc was this mentioned?
+    // TODO: Anyone know WTF is going on in the hardware that the "unused" bits need their values preserved? Are they used as scratch space or some shit?
+    uint32_t fsel_paddr = get_gpio_fsel_paddr(pin);
+    uint32_t old_value = GET32(fsel_paddr);
+    uint32_t bits_to_clear = 0b111 << ((pin % 10) * 3);
+    uint32_t bits_to_set = 0b001 << ((pin % 10) * 3);
+    uint32_t new_value = (old_value & ~bits_to_clear) | bits_to_set;
+    PUT32(fsel_paddr, new_value);
 }
 
 // set GPIO <pin> on.
 void gpio_set_on(unsigned int pin) {
-    // implement this
-    // use <gpio_set0>
+    PUT32(gpio_set0, (1 << pin));
 }
 
 // set GPIO <pin> off
 void gpio_set_off(unsigned int pin) {
-    // implement this
-    // use <gpio_clr0>
+    PUT32(gpio_clr0, (1 << pin));
 }
 
 // set <pin> to <v> (v \in {0,1})
