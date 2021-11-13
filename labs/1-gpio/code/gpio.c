044fe2e08bdf09736589c1aa297ed92d438f8e0c
Shane Kadish <skadish@atlassian.com>
2021-11-13 13:48:07 +1100

    add gpio_fsel0, replace unsigned with unsigned int for clarity

--- labs/1-gpio/code/gpio.c
+++ labs/1-gpio/code/gpio.c
@@ -10,10 +10,11 @@
 #include "rpi.h"
 
 // see broadcomm documents for magic addresses.
-#define GPIO_BASE 0x20200000
-static const unsigned gpio_set0  = (GPIO_BASE + 0x1C);
-static const unsigned gpio_clr0  = (GPIO_BASE + 0x28);
-static const unsigned gpio_lev0  = (GPIO_BASE + 0x34);
+#define GPIO_BASE_PADDR 0x20200000
+static const unsigned int gpio_fsel0 = (GPIO_BASE_PADDR);
+static const unsigned int gpio_set0  = (GPIO_BASE_PADDR + 0x1C);
+static const unsigned int gpio_clr0  = (GPIO_BASE_PADDR + 0x28);
+static const unsigned int gpio_lev0  = (GPIO_BASE_PADDR + 0x34);
 
 //
 // Part 1 implement gpio_set_on, gpio_set_off, gpio_set_output
@@ -23,25 +24,25 @@ static const unsigned gpio_lev0  = (GPIO_BASE + 0x34);
 //
 // note: fsel0, fsel1, fsel2 are contiguous in memory, so you 
 // can (and should) use array calculations!
-void gpio_set_output(unsigned pin) {
+void gpio_set_output(unsigned int pin) {
     // implement this
     // use <gpio_fsel0>
 }
 
 // set GPIO <pin> on.
-void gpio_set_on(unsigned pin) {
+void gpio_set_on(unsigned int pin) {
     // implement this
     // use <gpio_set0>
 }
 
 // set GPIO <pin> off
-void gpio_set_off(unsigned pin) {
+void gpio_set_off(unsigned int pin) {
     // implement this
     // use <gpio_clr0>
 }
 
 // set <pin> to <v> (v \in {0,1})
-void gpio_write(unsigned pin, unsigned v) {
+void gpio_write(unsigned int pin, unsigned int v) {
     if(v)
        gpio_set_on(pin);
     else
@@ -53,13 +54,13 @@ void gpio_write(unsigned pin, unsigned v) {
 //
 
 // set <pin> to input.
-void gpio_set_input(unsigned pin) {
+void gpio_set_input(unsigned int pin) {
     // implement.
 }
 
 // return the value of <pin>
-int gpio_read(unsigned pin) {
-    unsigned v = 0;
+int gpio_read(unsigned int pin) {
+    unsigned int v = 0;
 
     // implement.
     return v;
