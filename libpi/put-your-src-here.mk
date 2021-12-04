61db60235adb7c5826ca1bed517634f1b8e78458
Shane Kadish <skadish@atlassian.com>
2021-12-04 15:04:58 +1100

    hopefully lab 2 is ok

--- libpi/put-your-src-here.mk
+++ libpi/put-your-src-here.mk
@@ -6,10 +6,8 @@
 # put the path to any .S files.
 YOUR_ASM = 
 # put the path to any .c files
-YOUR_SRC = src/hello.c
+YOUR_SRC = src/hello.c src/gpio.c
 
 # these are initial .o's we give you: to use your own, remove them
 # and add yours (to YOUR_OBJS)
 STAFF_OBJS= staff-objs/uart.o       \
-            staff-objs/gpio.o       \
-            staff-objs/gpio-pud.o 
